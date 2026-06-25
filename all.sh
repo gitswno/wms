e7a45547f34b45c4cf5a8f19fed51fe6f79fb72e
oneplus <hyhlzi@foxmail.com>
2026-06-26 00:03:35 +0800

    2026-06-26 00:03:35

--- all.sh
+++ all.sh
@@ -5,13 +5,24 @@ count=$(( $(git rev-list --count HEAD) - 3 ))
 if [ $count -gt 0 ]; then
   > all_wms.txt  # 清空或创建 all_wms.txt 文件，防止历史数据干扰
   for i in $(git rev-list HEAD --max-count=$count); do
+	echo "版本ID: $i" >>all_wms.txt
+	echo -n '时间: ' >>all_wms.txt
+	git show -s --format=%cd --date=format:'%Y-%m-%d %H:%M:%S' "$i" >> all_wms.txt
     git show "$i":wms.txt >> all_wms.txt
+	echo >>all_wms.txt
+	echo >>all_wms.txt
   done
   
   # --skip=$count 跳过前面已经处理的版本，--max-count=2 只取最后的2个
   for i in $(git rev-list HEAD --skip=$count --max-count=2); do
+	echo "版本ID: $i" >>all_wms.txt
+	echo -n '时间: ' >>all_wms.txt
+	git show -s --format=%cd --date=format:'%Y-%m-%d %H:%M:%S' "$i" >> all_wms.txt
     git show "$i":简易仓库系统.txt >> all_wms.txt
+	echo >>all_wms.txt
+	echo >>all_wms.txt
   done
 else
   echo "总版本数不超过2个，无法分割。"
 fi
+
